ee4e405bffa84c4fa49fc5f97dafc63b48bee74e
Aleksandar Kamburov <kamburoff@gmail.com>
2023-06-28 02:39:32 +0300

    start again, delete all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AngularFireModule } from "@angular/fire/compat";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AddBookComponent } from './components/add-book/add-book.component';
-import { BookComponent } from './components/book/book.component';
 import {environment} from "../environments/environment";
 import {FormsModule} from "@angular/forms";
 
@@ -17,10 +13,6 @@ import {FormsModule} from "@angular/forms";
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    AddBookComponent,
-    BookComponent,
   ],
   imports: [
     BrowserModule,
